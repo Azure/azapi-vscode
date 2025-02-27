2049ca705dbbe44ffed0ceba2a73a3a7525b8108
Heng Lu <henglu@microsoft.com>
2025-02-27 15:47:41 +0800

    fix lint

--- src/survey.ts
+++ src/survey.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { config } from './vscodeUtils';
 
 export async function ShouldShowSurvey(): Promise<boolean> {
-  var currentConfig: any = config('azapi').get('survey');
+  let currentConfig: any = config('azapi').get('survey');
   if (
     currentConfig == undefined ||
     currentConfig.surveyPromptDate == undefined ||
@@ -10,7 +10,7 @@ export async function ShouldShowSurvey(): Promise<boolean> {
   ) {
     currentConfig = {};
     // first time, remind after 10 days
-    var promptDate = new Date();
+    const promptDate = new Date();
     promptDate.setDate(promptDate.getDate() + 10);
     currentConfig.surveyPromptDate = promptDate.toISOString();
     currentConfig.surveyPromptIgnoredCount = 0;
@@ -22,8 +22,8 @@ export async function ShouldShowSurvey(): Promise<boolean> {
     return false;
   }
 
-  var currentDate = new Date();
-  var promptDate = new Date(currentConfig.surveyPromptDate);
+  const currentDate = new Date();
+  const promptDate = new Date(currentConfig.surveyPromptDate);
   if (currentDate >= promptDate) {
     return true;
   }
@@ -35,21 +35,22 @@ export async function ShowSurvey(): Promise<void> {
   const reloadMsg =
     'Looks like you are using Terraform AzAPI Provider. We’d love to hear from you! Could you help us improve product usability by filling out a 2-3 minute survey about your experience with it?';
   const selected = await vscode.window.showInformationMessage(reloadMsg, 'Yes', 'Not Now', 'Never');
-  var currentConfig: any = config('azapi').get('survey');
+  let currentConfig: any = config('azapi').get('survey');
   if (currentConfig == undefined) {
     currentConfig = {};
     currentConfig.surveyPromptDate = 'none';
     currentConfig.surveyPromptIgnoredCount = 0;
   }
 
+  const nextPromptDate = new Date();
+
   switch (selected) {
     case 'Yes':
       vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('https://aka.ms/AzAPI2025'));
       // reset the survey prompt date and ignored count, remind after 180 days
       currentConfig.surveyPromptIgnoredCount = 0;
-      var promptDate = new Date();
-      promptDate.setDate(promptDate.getDate() + 180);
-      currentConfig.surveyPromptDate = promptDate.toISOString();
+      nextPromptDate.setDate(nextPromptDate.getDate() + 180);
+      currentConfig.surveyPromptDate = nextPromptDate.toISOString();
       break;
     case 'Never':
       currentConfig.surveyPromptDate = 'never';
@@ -60,14 +61,12 @@ export async function ShowSurvey(): Promise<void> {
       currentConfig.surveyPromptIgnoredCount++;
       if (currentConfig.surveyPromptIgnoredCount == 1) {
         // first time ignore, remind after 7 days
-        var promptDate = new Date();
-        promptDate.setDate(promptDate.getDate() + 7);
-        currentConfig.surveyPromptDate = promptDate.toISOString();
+        nextPromptDate.setDate(nextPromptDate.getDate() + 7);
+        currentConfig.surveyPromptDate = nextPromptDate.toISOString();
       } else {
         // second time ignore, remind after 30 days
-        var promptDate = new Date();
-        promptDate.setDate(promptDate.getDate() + 30);
-        currentConfig.surveyPromptDate = promptDate.toISOString();
+        nextPromptDate.setDate(nextPromptDate.getDate() + 30);
+        currentConfig.surveyPromptDate = nextPromptDate.toISOString();
       }
       break;
   }
